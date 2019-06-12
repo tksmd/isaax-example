9cbf912e7b39eb2a6d9c7f0d6a4a3a04e7ace8c9
tksmd <someda@isenshi.com>
2019-06-12 23:23:20 +0900

    update test

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import time
 
-while True:
-	print("Hello IoT from Isaax")
-	time.sleep(5)
+import os
+
+
+def main():
+    interval = os.environ.get('APP_SLEEP_INTERVAL', 5)
+    while True:
+        print('Hello IoT from Isaax {0}'.format(interval))
+        time.sleep(interval)
+
+
+if __name__ == '__main__':
+    main()
